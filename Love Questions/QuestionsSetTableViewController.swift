2f2d55c88164ee0cacbbf2adf31b81d8e6ede7b3
Maciej Sienkiewicz <maciej@macoscope.com>
2021-01-28 02:53:51 +0100

    Added ability to select given set and open view with questions

--- Love Questions/QuestionsSetTableViewController.swift
+++ Love Questions/QuestionsSetTableViewController.swift
@@ -10,13 +10,17 @@ import UIKit
 class QuestionsSetTableViewController: UITableViewController {
 
     var questionsSets = [QuestionsSet]()
-    
+    var dataController: DataController!
+
     public func setup(with sets: [QuestionsSet]) {
         self.questionsSets = sets
     }
     
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        self.dataController = DataController()
+        self.setup(with: self.dataController.questionsSets)
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -81,14 +85,12 @@ class QuestionsSetTableViewController: UITableViewController {
     }
     */
 
-    /*
     // MARK: - Navigation
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+        if let destination = segue.destination as? QuestionsSetViewController {
+            destination.setup(with: self.questionsSets[self.tableView.indexPathForSelectedRow!.row])
+        }
     }
-    */
 
 }
